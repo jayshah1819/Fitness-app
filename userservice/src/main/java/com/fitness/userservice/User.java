972e560d93e786f9a8240078c36d862b24081ef0
jayshah1819 <jayshah.jk.jk18@gmail.com>
2025-04-16 18:49:49 -0400

    Initial commit

---
+++ userservice/src/main/java/com/fitness/userservice/User.java
@@ -0,0 +1,44 @@
+package com.fitness.userservice;
+
+import com.fitness.userservice.model.UserRole;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="users")
+@Data //for the getters and setters and required args constructor and
+// toString and equalsAndHashcode
+
+
+public class User{
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+
+    @Column(unique=true, nullable=false)
+    private String email;
+
+
+    @Column(nullable=false)
+    private String password;
+
+    private String firstName;
+    private String lastName;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role=UserRole.USER;// WE WILL SEE THIS ERROR BECAUSE WE HAVE TO CREATE ENUM FOR THIS FORR
+    //WE HAVE TO GO TO HOVER OVER IT AND IN MORE ACTIONS IT SHOULD BE ENUM ADD AND HAVE
+    //ADDED IT IN PACKAGE CALL FITNESS.MODEL SOMETHING LIKE THIS BUT FOCUS SHOULD BE ON THE MODEL.
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+
+}
---
+++ userservice/src/main/java/com/fitness/userservice/model/UserRole.java
@@ -0,0 +1,8 @@
+package com.fitness.userservice.model;
+
+public enum UserRole {
+    USER, ADMIN;
+    //AFTER THAT WE HAVE TO ADD USER ADMIN HERE.
+    //This provides Type safety ,Code Completion & clarity and cleaner Database and easy to access and maintain.
+
+}
