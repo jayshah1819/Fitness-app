e6962b8340eebfd2bfa2f242e04b34b3b0ae6cb3
jayshah1819 <jayshah.jk.jk18@gmail.com>
2025-05-03 13:58:50 -0400

    Made some changes to solve errors

--- activityservice/src/main/java/com/fitness/activityservice/ActivityRepository.java
+++ activityservice/src/main/java/com/fitness/activityservice/ActivityRepository.java
@@ -1,12 +1,14 @@
 package com.fitness.activityservice;
 
 import com.fitness.activityservice.model.Activity;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
+@EnableMongoAuditing
 
 public interface ActivityRepository extends MongoRepository<Activity, String> {
 
--- activityservice/src/main/java/com/fitness/activityservice/dto/ActivityRequest.java
+++ activityservice/src/main/java/com/fitness/activityservice/dto/ActivityRequest.java
@@ -5,7 +5,6 @@ import com.fitness.activityservice.model.ActivityType;
 import lombok.Data;
 
 import java.time.LocalDateTime;
-import java.util.HashMap;
 import java.util.Map;
 
 @Data
