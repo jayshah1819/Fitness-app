5c656d7eb571823fa764b38182c03175c5fbad0a
jayshah1819 <jayshah.jk.jk18@gmail.com>
2025-05-13 16:05:09 -0400

    Updated Mongodb connection.

---
+++ aiservice/src/main/java/com/fitness/aiservice/config/MongoConfig.java
@@ -0,0 +1,14 @@
+package com.fitness.aiservice.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@Configuration
+@EnableMongoAuditing
+//MongoDB Configuration issue
+public class MongoConfig {
+
+
+
+}
