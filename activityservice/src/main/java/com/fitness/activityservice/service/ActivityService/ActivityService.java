133c0cffe579cc51469e5a79628bb9929f3cbbcf
jayshah1819 <jayshah.jk.jk18@gmail.com>
2025-05-14 17:11:48 -0400

    added rabbitmq into activity service

--- activityservice/src/main/java/com/fitness/activityservice/service/ActivityService/ActivityService.java
+++ activityservice/src/main/java/com/fitness/activityservice/service/ActivityService/ActivityService.java
@@ -5,6 +5,9 @@ import com.fitness.activityservice.dto.ActivityRequest;
 import com.fitness.activityservice.dto.ActivityResponse;
 import com.fitness.activityservice.model.Activity;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,8 +16,20 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 
+@Slf4j
+
 public class ActivityService {
     private final ActivityRepository activityRepository;
+    private final RabbitTemplate rabbitTemplate;
+
+//Don't forget we have to add this inside of the function
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
+
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
+
     private final UserValidationService userValidationService;
     public ActivityResponse trackActivity(ActivityRequest request) {
 
@@ -28,6 +43,17 @@ public class ActivityService {
                 .build();
 
         Activity savedActivity = activityRepository.save(activity);
+
+        //publish to mq for ai processing
+        try {
+            rabbitTemplate.convertAndSend(exchange, routingKey, savedActivity);
+
+        } catch(Exception e){
+            log.error("failed to publish activity using Rabbitmq", e);
+
+        }
+
+
         return mapToResponse(savedActivity);
 
 
