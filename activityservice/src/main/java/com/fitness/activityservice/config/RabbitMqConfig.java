e5c2aef593e34321630e12eb41e6a97b2f66396e
jayshah1819 <jayshah.jk.jk18@gmail.com>
2025-05-14 16:27:06 -0400

    update on rabbitmq

--- activityservice/src/main/java/com/fitness/activityservice/config/RabbitMqConfig.java
+++ activityservice/src/main/java/com/fitness/activityservice/config/RabbitMqConfig.java
@@ -1,4 +1,21 @@
 package com.fitness.activityservice.config;
 
-public class RabbitMqConfig {
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfig{
+
+    @Bean
+    public Queue activityQueue() {
+        return new Queue("activity.queue", true);
+    }
+
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
 }
