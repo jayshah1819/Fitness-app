f87303a4b2236f2779e018c5d91ab2cd20db7854
jayshah1819 <jayshah.jk.jk18@gmail.com>
2025-05-17 15:03:43 -0400

    Update GeminiService.java

--- aiservice/src/main/java/com/fitness/aiservice/service/GeminiService.java
+++ aiservice/src/main/java/com/fitness/aiservice/service/GeminiService.java
@@ -1,5 +1,31 @@
 package com.fitness.aiservice.service;
 
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Map;
+
 public class GeminiService {
     private final WebClient webClient;
+
+    
+    public GeminiService(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.build();
+
+    }
+
+    public String getAnswer(String question) {
+        Map<String, Object> request = Map.of(
+
+                "contents", new Object[]{
+                        Map.of(
+                                "parts", new Object[]{
+                                        Map.of("text", question)
+
+                                }
+                        )
+                }
+        );
+
+        return request.toString();
+    }
 }
